388281dced8f699e0a485c720879d34ea1d407d7
Peet van de Sande <peet@peetvandesande.com>
2024-04-28 00:59:24 +0200

    First working check-in

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # nac-prereq
 Prerequisites for Network as Code
+
+Currently deploys a set of Docker containers for phpipam.
+Username: Admin
+Password: ipamadmin
+If the above don't work, look here: https://phpipam.net/demo/
---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "ipam_db_user" {
+	description = "DB username for phpipam account"
+	type = string
+	default = "phpipam"
+}
+
+variable "ipam_db_name" {
+  description = "DB name for ipam app"
+  type = string
+  default = "phpipam"
+}
